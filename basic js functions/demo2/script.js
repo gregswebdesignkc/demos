0055cf933849fbf3395b1348a6871216ec551d1a
gpokingpin66203 <79721812+gpokingpin66203@users.noreply.github.com>
2021-04-16 09:36:20 -0500

    submit all demos to repos

---
+++ basic js functions/demo2/script.js
@@ -0,0 +1,5 @@
+// Write a function that accepts an array of numbers as an argument. This function should loop through the array and sum all of the odd numbers. Return the sum of all odd numbers.
+
+/*********FUNCTIONS ADDITIONAL PRACTICE - MICHAEL MEECHAM *******/
+array [125, 97, 63, 244, 98, 102, 65, 77, 93]
+
